aeefa0d930f4ef897dce4b20dfca86214454b77b
You9123 <you9123chacon@gmail.com>
2026-05-24 20:53:56 -0600

    Creacion de Payments dentro de models.py

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -26,4 +28,89 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    #holaaa
+class Payments(models.Model):
+    
+    CARD_CHOICES = [
+        ('VISA', 'Visa'), 
+        ('MASTERCARD', 'MasterCard')
+        ('AMEX', 'American Express')
+    ]
+    
+    user = models.CharField(
+        max_length=50,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$',
+                message='Solo se permiten letras y espacios.' 
+            )
+        ]
+    )
+    
+    cardType = models.CharField(
+        max_length=20,
+        choices=CARD_CHOICES
+    )
+    
+    cardNumber = models.IntegerField(
+        max_length=16
+    )
+    
+    expirationDate = models.CharField(
+        max_length=5,
+        validators=[
+            RegexValidator(
+                regex=r'^(0[1-9]|1[0-2])\/([0-9]{2})$',
+                message='Formato inválido. Use MM/YY'
+            )
+        ]
+    )
+    
+    cvv = models.CharField(
+        max_length=4
+    )
+    
+    def clean(self):
+        
+        #VISA
+        if self.cardType == 'VISA':
+            
+            if len(self.cardNumber) != 16:
+                raise ValidationError(
+                    "Visa requiere exactamente 16 digitos."
+                )
+                
+            if len(self.cvv) != 3:
+                raise ValidationError(
+                    "Visa usa CVV de 3 digitos."
+            )
+            
+        #Mastercard
+        elif self.cardType == 'MASTERCARD':
+            
+            if len(self.cardNumber) != 16:
+                raise ValidationError(
+                    "MasterCard requiere 16 dígitos."
+                )
+
+            if len(self.cvv) != 3:
+                raise ValidationError(
+                    "MasterCard usa CVV de 3 dígitos."
+                )
+                
+        #American Express
+        elif self.cardType == 'AMEX':
+
+            if len(self.cardNumber) != 15:
+                raise ValidationError(
+                    "AMEX requiere 15 dígitos."
+                )
+
+            if len(self.cvv) != 4:
+                raise ValidationError(
+                    "AMEX usa CVV de 4 dígitos."
+                )
+
+
+    def __str__(self):
+        return f"{self.user} - {self.cardType}"
+    
